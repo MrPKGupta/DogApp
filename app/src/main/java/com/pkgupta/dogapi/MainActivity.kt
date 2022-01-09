c2b8d4d14437be821b6154707de8e49cf70938b9
PK Gupta <pukgupta@in.ibm.com>
2022-01-10 03:04:35 +0530

    Setup for Ktor, Coil

--- app/src/main/java/com/pkgupta/dogapi/MainActivity.kt
+++ app/src/main/java/com/pkgupta/dogapi/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.pkgupta.dogapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.pkgupta.dogapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadRandomDog()
+    }
+
+    private fun loadRandomDog() {
+        binding.ivDog.load("https://images.dog.ceo/breeds/terrier-american/n02093428_5165.jpg")
     }
 }
---
+++ app/src/main/java/com/pkgupta/dogapi/RandomDogApi.kt
@@ -0,0 +1,5 @@
+package com.pkgupta.dogapi
+
+import io.ktor.client.*
+
+class RandomDogApi(private val client: HttpClient)
